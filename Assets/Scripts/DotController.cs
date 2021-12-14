ffac39d362e50d1df06bbf24b05035829fe6ea8a
Naufal Shidqi <naufalshidqi99@gmail.com>
2021-12-14 22:21:40 +0700

    Build v2

---
+++ Assets/Scripts/DotController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [SerializeField]
+    private float xForceRange;
+    [SerializeField]
+    private float yForceRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        Invoke("Push", 1);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void Push()
+    {
+        //Randomize force
+        float yRandomForce = Random.Range(-yForceRange, yForceRange);
+        float xRandomForce = Random.Range(-xForceRange, xForceRange);
+
+        //Direction
+        Vector2 direction = Vector2.zero;
+        direction = new Vector2(xRandomForce, yRandomForce);
+
+        //Add force to rb
+        rb2D.AddForce(direction);
+    }
+}
