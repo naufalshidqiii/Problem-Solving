055a19437eb5e67fc2e885d80e2613bea7a3a39e
Naufal Shidqi <naufalshidqi99@gmail.com>
2021-12-15 17:16:51 +0700

    Build v4

--- Assets/Scripts/DotController.cs
+++ Assets/Scripts/DotController.cs
@@ -7,19 +7,29 @@ public class DotController : MonoBehaviour
     private Rigidbody2D rb2D;
 
     [SerializeField]
-    private float xForceRange;
+    private float _xForceRange;
     [SerializeField]
-    private float yForceRange;
+    private float _yForceRange;
+    [SerializeField]
+    private float _moveSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        Invoke("Push", 1);
+        //Invoke("Push", 1);
     }
 
     // Update is called once per frame
     private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    private void FixedUpdate()
     {
         
     }
@@ -27,8 +37,8 @@ public class DotController : MonoBehaviour
     private void Push()
     {
         //Randomize force
-        float yRandomForce = Random.Range(-yForceRange - 50, yForceRange + 50);
-        float xRandomForce = Random.Range(-xForceRange - 50, xForceRange + 50);
+        float yRandomForce = Random.Range(-_yForceRange - 50, _yForceRange + 50);
+        float xRandomForce = Random.Range(-_xForceRange - 50, _xForceRange + 50);
 
         //Direction
         Vector2 direction = Vector2.zero;
@@ -36,5 +46,12 @@ public class DotController : MonoBehaviour
 
         //Add force to rb
         rb2D.AddForce(direction);
+    }
+
+    private void Move(float h, float v)
+    {
+        Vector2 direction = new Vector2(h, v).normalized;
+        direction = direction * _moveSpeed * Time.deltaTime;
+        rb2D.velocity = direction;
     }
 }
